016dacb6836a79c1511aa544843af35897de22f0
Xardas49 <XardasTheModder@interia.pl>
2018-05-14 23:33:30 +0200

    add missing face textures + design changes

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ var BodyTableMale = [0,1,2,3,8,9,10];
 var BodyTableFemale = [4,5,6,7,11,12];
 var HeadTableFemale = [0,1,2,3,4,5,6,7,8,9,10];
 var HeadTableMale = [11,12,13,14,15,16];
-var FaceTableMale0 = [41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,19];
-var FaceTableMale1 = [58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75];
-var FaceTableMale2 = [120,121,122,123,124,125,126,127,128];
-var FaceTableMale3 = [129,130,131,132,133,134,135,136];
-var FaceTableFemale = [137,138,139,140,141,142,143,144,145,146,148,149,150,151,152,153,154,155,156,157];
+var FaceTableMale0 = [19,39,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,19];
+var FaceTableMale1 = [0,1,2,3,5,6,7,9,10,13,14,16,18,20,21,22,23,24,25,26,27,31,32,33,34,35,36,37,38,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119];
+var FaceTableMale2 = [15,29,30,40,120,121,122,123,124,125,126,127,128];
+var FaceTableMale3 = [4,11,12,17,28,129,130,131,132,133,134,135,136];
+var FaceTableFemale = [137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157];
 var currFaceTable;
 var currBodyTable;
 var currHeadTable;
@@ -29,7 +29,7 @@ var noop = function() {};
 var renderGif = noop;
 if (!window.location.hash)
 {
-	window.location.hash = "2-5-0-0-0-true";
+	window.location.hash = "2-1-0-0-0-true";
 }
 if (window.location.hash) {
 	permalink = window.location.hash.substring(1).split('-');
